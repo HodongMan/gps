8eb3e42bf8642af5f5ded3d4c487c29b0c6e341d
hodongman <arshavin3@naver.com>
2017-09-21 14:36:15 +0900

    first components update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
+import {Navbar} from './components';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Navbar/>
+        );
+    }
 }
 
 export default App;
